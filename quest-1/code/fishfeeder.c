3001d2bd529b4cbd1c25f3af0db10b7e2adf221e
brian-macomber <52986821+brian-macomber@users.noreply.github.com>
2020-09-22 03:39:58 -0400

    more planning

--- quest-1/code/fishfeeder.c
+++ quest-1/code/fishfeeder.c
@@ -3,6 +3,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+int counter;
+
 void init()
 {
     // congifure init stuff here
@@ -10,6 +12,12 @@ void init()
 
 void app_main(void)
 {
+    //Should use global variable as a counter just like in skill11
+
+    // The overall idea can be divided into 3 tasks
+    // - servo task
+    // - timer task
+    // - alphanumeric display
 
     init();
     //create tasks here for servo, timer and alphnumeric display
