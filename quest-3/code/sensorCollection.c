b5e5700a8ce777a8512bd83f914366dbae11a335
brian-macomber <52986821+brian-macomber@users.noreply.github.com>
2020-10-22 14:58:28 -0400

    initial commit

---
+++ quest-3/code/sensorCollection.c
@@ -0,0 +1,9 @@
+
+/*
+Here is where all of the esp modules should be:
+- i2c accelerometer module to get accel data (ROLL and PITCH)
+- Thermistor module to read temperature
+- UDP module that sends data over a socket to the Node.js server
+ - send roll, pitch, temperature
+ - recieve LED intensity
+*/
---
+++ quest-3/code/server.js
@@ -0,0 +1,3 @@
+// Here is where all of the front end stuff will happen
+//- Create socket with UDP to talk to esp (recieve sensor data / send intensity data)
+//- Create web page (can take code from prior skill, change port number)
